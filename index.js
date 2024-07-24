5d2b5106034e94777f3c7f6bd57c3a01c9c685a4
Ryotankobu <ucp4fya7@yahoo.co.jp>
2024-07-24 16:33:31 +1000

    test1

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 console.log("I caused a merge conflict");
 
 function calculate(a, b) {
-    return a + b
+  return a + b;
 }
 
+answer = calculate(200, 200);
+print(answer);
 
-answer = calculate(100, 100)
-print(answer)
-
-console.log("this is a conflict3")
-console.log("this is a conflict2")
-
-
+console.log("this is a conflict3");
+console.log("this is a conflict2");
