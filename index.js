2550518b1a8968875550376bf8ddd7802038bff6
Ryotankobu <ucp4fya7@yahoo.co.jp>
2024-07-25 08:40:13 +1000

    preparation for lab2

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-console.log("I caused a merge conflict");
-console.log("adding one more conflict");
+console.log("Hello World");
 
 function calculate(a, b) {
   return a + b;
 }
 
-answer = calculate(200, 200);
+answer = calculate(300, 300);
 print(answer);
-
-answer = calculate(100, 100)
-print(answer)
-
-console.log("I deleted 2 lines then added this line")
-
